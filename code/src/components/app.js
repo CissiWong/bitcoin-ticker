480078a85c65a53e8b240481bc31a200fca0c445
Cecilia Wong <cissi.wong@gmail.com>
2018-01-04 10:38:06 +0100

    added library and visual

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { LineChart, Line, Tooltip, YAxis } from "recharts"
 import openGdaxWebsocket from "../gdax-websocket"
 
 class App extends React.Component {
@@ -19,23 +20,33 @@ class App extends React.Component {
   }
 
   handleNewTickerMessage = newTickerMessage => {
+    newTickerMessage.price = parseFloat(newTickerMessage.price, 10)
+    //turns the string of price to integer//
     this.setState(previousState => ({
       tickerMessages: previousState.tickerMessages.concat([newTickerMessage])
     }))
   }
 
+  // render() {
+  //   return (
+  //     <div>
+  //       {this.state.tickerMessages.map(msg => (
+  //         <div key={msg.sequence}>
+  //           {msg.time}: <strong>{msg.price} EUR</strong>
+  //         </div>
+  //       ))}
+  //     </div>
+  //   )
+  // }
+
   render() {
     return (
-      <div>
-        {this.state.tickerMessages.map(msg => (
-          <div key={msg.sequence}>
-            {msg.time}: <strong>{msg.price} EUR</strong>
-          </div>
-        ))}
-      </div>
+      <LineChart width={400} height={100} data={this.state.tickerMessages}>
+        <Line type="monotone" dataKey="price" stroke="#8884d8" strokeWidth={2} />
+        <YAxis type="number" domain={["dataMin", "dataMax"]} />
+        <Tooltip />
+      </LineChart>
     )
   }
-
 }
-
 export default App
